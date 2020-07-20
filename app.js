081c7a9007c2103b4663e9cc44443afba45076c9
Juan Carlos Gonzalez <juancarlos@digitalhouse.com>
2020-07-20 14:48:39 -0300

    moving files

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const movieRouter = require('./routes/movies');
-const userRouter = require('./routes/users');
-const omdbRouter = require('./routes/omdb');
+const indexRouter = require('./site/routes/index');
+const authRouter = require('./site/routes/auth');
+const movieRouter = require('./site/routes/movies');
+const userRouter = require('./site/routes/users');
+const omdbRouter = require('./site/routes/omdb');
 
-const sessionMdw = require('./middlewares/session');
-const rememberMdw = require('./middlewares/remember');
+const sessionMdw = require('./site/middlewares/session');
+const rememberMdw = require('./site/middlewares/remember');
 
 const app = express();
 
 // view engine setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname , 'views'));
+app.set('views', path.join(__dirname , 'site', 'views'));
 
 app.use(session({
   secret : 'Digital Movies',
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(rememberMdw);
 
 //por defecto express me lo configura
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/public'));
 
 //use los method put y delete en las rutas y el formulario
 app.use(methodOverride('_method'));
